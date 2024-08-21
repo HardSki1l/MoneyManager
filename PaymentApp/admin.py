62d21e70531608a7bd928fa4e0e0a88d427ee741
omon212 <omonnuloraimkulov@gmail.com>
2024-08-21 19:16:44 +0500

    add requirements.txt and change admin py

--- PaymentApp/admin.py
+++ PaymentApp/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import HistoryModel
 
-
-# Register your models here.
-
 class SearchHistoryModel(admin.ModelAdmin):
+    exclude = ('who_payed',)
     list_display = ['card_related', 'who_payed', 'price', 'when_date', 'where']
 
 
